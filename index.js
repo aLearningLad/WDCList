ba4c1406d7c070117c97b0f7ff4f3cb2c51ec424
aLearningLad <mocmanca@gmail.com>
2024-08-22 04:06:23 +0200

    setting up express app, start & dev scripts

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// nothing to see here...yet!
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`server is listening here: http://localhost:${process.env.PORT}`);
+});
