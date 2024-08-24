5c5273f006d48c50f9f44e9d80ccb7698acb5eda
aLearningLad <mocmanca@gmail.com>
2024-08-24 17:08:15 +0200

    adding error handling constants

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const userRoutes = require("./routes/userRoutes");
+const connectToDb = require("./lib/utils/connectToDb");
+const errorHandler = require("./lib/utils/middleware/errorHandler");
 
 const app = express();
 app.use(express.json());
 app.use("/", userRoutes);
+app.use(errorHandler);
+connectToDb();
 
 app.listen(process.env.PORT, () => {
   console.log(`server is listening here: http://localhost:${process.env.PORT}`);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const connectToDb = require("../lib/utils/connectToDb");
 
 // add a driver entry
 // @POST /api/user
